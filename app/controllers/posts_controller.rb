83115f6990ce16ed61a45f5119db3bd5bf550cd2
xiaowei <shenzhoudance@gmail.com>
2018-10-05 15:41:58 +0800

    edit destory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,40 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post
-  end
+      if @post.save
+        redirect_to @post
+      else
+        render 'new'
+      end
+    end
 
   def show
    @post = Post.find(params[:id])
   end
+
+  def edit
+   @post = Post.find(params[:id])
+  end
+
+  def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+  def destroy
+     @post = Post.find(params[:id])
+     @post.destroy
+
+     redirect_to posts_path
+  end
+
+
 
 private
    def post_params
